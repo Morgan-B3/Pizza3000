256ecb885b6fb57d8be1c2c77165f376c950a2ca
Morgan-B3 <boutroism@gmail.com>
2024-01-20 23:53:07 +0100

    optimized menus icons

--- pizza3000/src/components/Menu.js
+++ pizza3000/src/components/Menu.js
@@ -1,28 +1,15 @@
 import { Card } from 'antd'
 import React from 'react'
-import { FaShoppingCart, } from "react-icons/fa";
-import { FaFireFlameCurved } from "react-icons/fa6";
-import { LuClipboardSignature } from "react-icons/lu";
 
 
 const Menu = ({action, title, subtitle, icon, color}) => {
-    let iconItem;
-    
-    if (icon === "<FaFireFlameCurved />"){
-        iconItem = <FaFireFlameCurved size={50}/>
-    } else if (icon === "<FaShoppingCart />") {
-        iconItem = <FaShoppingCart size={50}/>
-    } else if (icon === "<LuClipboardSignature />") {
-        iconItem = <LuClipboardSignature size={50}/>
-    }
-    
     const className = `${color} card`;
 
     return (
         <section onClick={action} className={className} >
             <div className='card-body'>
                 <span className='icon'>    
-                    {iconItem}
+                    {icon}
                 </span>
                 <h2>{title}</h2>        
                 <p>{subtitle}</p>   
--- pizza3000/src/menus.js
+++ pizza3000/src/menus.js
@@ -1,20 +1,24 @@
+import { FaShoppingCart, } from "react-icons/fa";
+import { FaFireFlameCurved } from "react-icons/fa6";
+import { LuClipboardSignature } from "react-icons/lu";
+
 const menus = [
     {
-        icon : "<LuClipboardSignature />",
+        icon : <LuClipboardSignature size={50} />,
         title: "Nouvelle commande",
         subtitle: "Créer et enregistrer une nouvelle commande",
         color: "blue",
         slug: "nouvelle-commande",
     },
     {
-        icon : "<FaFireFlameCurved />",
+        icon : <FaFireFlameCurved size={50} />,
         title: "Commandes en cours",
         subtitle: "Voir le détail des commandes en cours",
         color: "red",
         slug: "commandes",
     },
     {
-        icon : "<FaShoppingCart />",
+        icon : <FaShoppingCart size={50} />,
         title: "Paiement commande",
         subtitle: "Encaisser une commande",
         color: "green",
